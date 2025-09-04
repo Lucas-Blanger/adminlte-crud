d49889118c8ba20a7d4febafbba8a89d4dab83f0
Lucas-Blanger <blangerlucas@gmail.com>
2025-09-04 14:23:27 -0300

    modfy modularizing the page

---
+++ resources/views/parts/content-header.blade.php
@@ -0,0 +1,26 @@
+<div class="app-content-header">
+  <div class="row align-items-center">
+    <div class="col-sm-8">
+        @hasSection('page-title')
+      <h3 class="mb-0">@yield('page-title')</h3>
+      @endif
+    </div>
+
+
+        
+    
+    <div class="class=col-sm-8">
+        @isset($breadcrumb)
+      <ol class="breadcrumb mb-0">
+        @foreach ($breadcrumb as $breadcrumb)
+        <li class="breadcrumb-item"><a href="#">{{ $breadcrumb['label'] }}</a></li>
+        @endforeach
+      </ol>
+      @endisset
+    </div>
+
+    <div class="col-sm-auto ms-auto">
+      Actions
+    </div>
+  </div>
+</div>
