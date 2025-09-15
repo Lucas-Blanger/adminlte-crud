4a0e6d245dcfa3dc7f519861fbd4ed80df81c224
Lucas-Blanger <blangerlucas@gmail.com>
2025-09-15 14:10:31 -0300

    create fogot password

--- resources/views/auth/forgot_password.blade.php
+++ resources/views/auth/forgot_password.blade.php
@@ -1,1 +1,43 @@
-forgot_password
+@extends('layouts.auth')
+@section('body-class', 'login-page')
+@section('content')
+        <div class="login-box">
+      <div class="login-logo">
+        <a href="../index2.html"><b>Admin</b>LTE</a>
+      </div>
+      <!-- /.login-logo -->
+      <div class="card">
+        <div class="card-body login-card-body">
+          <p class="login-box-msg">Sign in to start your session</p>
+          <form action="{{ route('password.email') }}" method="post">
+            @csrf
+            <div class="input-group mb-3">
+                <div class="input-group-text"><span class="bi bi-envelope"></span></div>
+              <input type="email" name="email" class="form-control" @error('email') is-invalid @enderror placeholder="Email" value="{{ old('email') }}"/>
+             @error('email')
+                <div div class="invalid-feedback"> {{ $message }}</div>
+              @enderror
+            </div>
+
+
+            <div class="row">
+  <div class="col-12 text-center">
+    <div class="d-grid gap-2">
+      <button type="submit" class="btn btn-primary">Send me the link</button>
+    </div>
+  </div>
+  <!-- /.col -->
+</div>
+            <!--end::Row-->
+          </form>
+          <div class="mt-2 text-center">
+          <p class="mb-0">
+            <a href="register.html" class="text-center"> Back to Login </a>
+          </p>
+          </div>
+
+          
+        </div>
+      </div>
+    </div>    
+@endsection
